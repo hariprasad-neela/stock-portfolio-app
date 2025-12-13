1b690a54466b6e3f45197f6c345ae167cdc1d839
Hari Prasad <hariprasad.neela2008@gmail.com>
2025-12-13 14:44:21 +0530

    Forcing IP V4 to connect to Supabase

--- server/db.js
+++ server/db.js
@@ -6,7 +6,9 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false // Required for Supabase/Render connections
-    }
+    },
+    // ⬇️ ADD THIS LINE TO FORCE IPV4
+    family: 4
 });
 
 module.exports = pool;
---
+++ server/readme.md
@@ -0,0 +1,4 @@
+Backend API
+https://stock-portfolio-api-f38f.onrender.com
+Client
+https://stock-portfolio-app-kappa.vercel.app/
