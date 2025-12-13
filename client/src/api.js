b48242c1f0cfd860978bfa50a7019ea0b261efe9
Hari Prasad <hariprasad.neela2008@gmail.com>
2025-12-13 14:08:20 +0530

    Updated frontend API base URL to Render endpoint

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-    baseURL: 'http://localhost:5000', // Your Backend URL
+    //baseURL: 'http://localhost:5000', // Your Backend URL
+    baseURL: 'https://stock-portfolio-api-f38f.onrender.com/'
 });
 
 export default api;
