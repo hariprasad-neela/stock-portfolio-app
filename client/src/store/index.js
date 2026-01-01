4e65683226e19bff8b657813f701462f57bf58cb
Hari Prasad <hariprasad.neela2008@gmail.com>
2026-01-01 09:12:50 +0530

    Added Open Trades and stocks list Redux Slices

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,11 +3,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import portfolioReducer from './slices/portfolioSlice';
 import uiReducer from './slices/uiSlice';
 import ledgerReducer from './slices/ledgerSlice';
+import stocksReducer from './slices/stocksSlice';
+import tradesReducer from './slices/tradesSlice';
 
 export const store = configureStore({
   reducer: {
     portfolio: portfolioReducer,
     ui: uiReducer,
     ledger: ledgerReducer,
+    stocks: stocksReducer, 
+    trades: tradesReducer,
   },
 });
