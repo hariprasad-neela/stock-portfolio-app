12b77462b6b400bafb0f76ec69dc6b089672b7da
hariprasad-neela <163423467+hariprasad-neela@users.noreply.github.com>
2026-06-23 08:59:22 +0530

    Fix SQL query and improve error logging

--- server/controllers/candleController.js
+++ server/controllers/candleController.js
@@ -3,13 +3,13 @@ import pool from '../db.js';
 export const getCandles = async (req, res) => {
     try {
         const result = await pool.query(
-            "SELECT ticker FROM candle_data WHERE display = TRUE ORDER BY ticker ASC"
+            "SELECT *FROM candle_data WHERE stock='YESBANK' ORDER BY date ASC"
         );
         // Extracting just the strings into an array
         const tickers = result.rows.map(row => row.ticker);
         res.json(tickers);
     } catch (err) {
-        console.error("Error fetching Candle data:", err);
+        console.error("Error fetching Candle data --------:", err);
         res.status(500).json({ error: "Failed to fetch candle data" });
     }
 };
